d5d02efa85438a4b0903aa004f20ba21b265147e
Stefan Scekic <stafanscekic023@gmail.com>
2022-12-26 17:00:35 +0100

    Pocetak

---
+++ Replikator/Interface.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "Interface.h"
+
+void SendData(int ServiceID, void* data, int dataSize) {	
+	return;
+}
+
+void ReceiveData(void* data, int dataSize) {
+	printf("Poy");
+	return;
+}
---
+++ Replikator/Interface.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void SendData(int ServiceID, void* data, int dataSize);
+void ReceiveData(void* data, int dataSize);
